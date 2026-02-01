f0a529ecee99e342366f830559bdc123fe3b7c9f
Jakub Tatar <jakub.tatar4@gmail.com>
2026-02-01 17:34:57 +0100

    Hedgessssssss

--- main.py
+++ main.py
@@ -16,12 +16,33 @@ def load_map(filename, tile_size):
                 rect = pygame.Rect(x, y, tile_size, tile_size)
                 
                 if char == "1":
-                    tiles.append((rect, "1"))  # iba vizuálne, bez kolízie
+                    tiles.append((rect, "1"))  
                 elif char == "2":
                     tiles.append((rect, "2"))
-                    collidable_tiles.append(rect)  # iba 2-ky budú kolidovať
+                    collidable_tiles.append(rect) 
                 elif char == "0":
-                    tiles.append((rect, "0"))  # vizuálne, bez kolízie
+                    tiles.append((rect, "0"))  
+                elif char == "3":
+                    tiles.append((rect, "3"))
+                    collidable_tiles.append(rect)
+                elif char == "4":
+                    tiles.append((rect, "4"))
+                    collidable_tiles.append(rect)
+                elif char == "5":
+                    tiles.append((rect, "5"))
+                    collidable_tiles.append(rect)
+                elif char == "6":
+                    tiles.append((rect, "6"))
+                    collidable_tiles.append(rect)
+                elif char == "7":
+                    tiles.append((rect, "7"))
+                    collidable_tiles.append(rect)
+                elif char == "8":
+                    tiles.append((rect, "8"))
+                    collidable_tiles.append(rect)
+                elif char == "9":
+                    tiles.append((rect, "9"))
+                    collidable_tiles.append(rect)
     return tiles, collidable_tiles
 
 
@@ -37,7 +58,7 @@ pygame.display.set_caption("Kto druhemu jamu kope...")
 clock = pygame.time.Clock()
 
 # Create game objects
-player = Player(x=50, y=50, width=50, height=50, color=(0, 128, 255))
+player = Player(x=100, y=100, width=50, height=50, color=(0, 128, 255))
 camera = Camera(screen_width, screen_height)
 
 # Map settings
@@ -48,10 +69,25 @@ walls, collidable_walls = load_map(r".\Resources\Bitmaps\cmitermap.txt", TILE_SI
 tile1_img = pygame.image.load(r"Resources/Images/Image_Plot1.png").convert_alpha()
 tile2_img = pygame.image.load(r"Resources/Images/Image_Plot2.png").convert_alpha()
 
+# Load hedge images
+hedgefront_img = pygame.image.load(r"Resources/Images/Image_HedgeFront.png").convert_alpha()
+hedgetopfront_img = pygame.image.load(r"Resources/Images/Image_HedgeTopFront.png").convert_alpha()
+hedgeRD_img = pygame.image.load(r"Resources/Images/Image_HedgeCornerRD.png").convert_alpha()
+hedgeLD_img = pygame.image.load(r"Resources/Images/Image_HedgeCornerLD.png").convert_alpha()
+hedgeRT_img = pygame.image.load(r"Resources/Images/Image_HedgeCornerRT.png").convert_alpha()
+hedgeLT_img = pygame.image.load(r"Resources/Images/Image_HedgeCornerLT.png").convert_alpha()
+hedgetopwall_img = pygame.image.load(r"Resources/Images/Image_HedgeTopWall.png").convert_alpha()
+
 # Optionally scale to TILE_SIZE
 tile1_img = pygame.transform.scale(tile1_img, (TILE_SIZE, TILE_SIZE))
 tile2_img = pygame.transform.scale(tile2_img, (TILE_SIZE, TILE_SIZE))
-
+hedgefront_img = pygame.transform.scale(hedgefront_img, (TILE_SIZE, TILE_SIZE))
+hedgeRD_img = pygame.transform.scale(hedgeRD_img, (TILE_SIZE, TILE_SIZE))
+hedgeLD_img = pygame.transform.scale(hedgeLD_img, (TILE_SIZE, TILE_SIZE))
+hedgeRT_img = pygame.transform.scale(hedgeRT_img, (TILE_SIZE, TILE_SIZE))
+hedgeLT_img = pygame.transform.scale(hedgeLT_img, (TILE_SIZE, TILE_SIZE))
+hedgetopfront_img = pygame.transform.scale(hedgetopfront_img, (TILE_SIZE, TILE_SIZE))
+hedgetopwall_img = pygame.transform.scale(hedgetopwall_img, (TILE_SIZE, TILE_SIZE))
 
 # Main loop
 is_running = True
@@ -66,7 +102,7 @@ while is_running:
     camera.update(player)
 
     # CLEAR SCREEN FIRST
-    screen.fill((135, 206, 235))
+    screen.fill((8, 50, 20))
 
     # Draw tiles from the bitmapmap
     for rect, tile_type in walls:
@@ -74,6 +110,20 @@ while is_running:
             screen.blit(tile1_img, camera.apply(rect))
         elif tile_type == "0":
             screen.blit(tile2_img, camera.apply(rect))
+        elif tile_type == "3":
+            screen.blit(hedgefront_img, camera.apply(rect))
+        elif tile_type == "4":
+            screen.blit(hedgetopfront_img, camera.apply(rect))
+        elif tile_type == "5":
+            screen.blit(hedgeRD_img, camera.apply(rect))
+        elif tile_type == "6":
+            screen.blit(hedgeLD_img, camera.apply(rect))
+        elif tile_type == "7":
+            screen.blit(hedgetopwall_img, camera.apply(rect))
+        elif tile_type == "8":
+            screen.blit(hedgeLT_img, camera.apply(rect))
+        elif tile_type == "9":
+            screen.blit(hedgeRT_img, camera.apply(rect))
 
     # Draw player
     pygame.draw.rect(
